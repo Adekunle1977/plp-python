d8fb34c95f2308a16d2e9ead1bfc55b4971ee689
Adekunle <ak77mo@gmail.com>
2025-08-31 09:29:59 +0100

    Add files via upload

---
+++ my_list.py.py
@@ -0,0 +1,28 @@
+july_cohort =[]
+
+july_cohort.append("10")
+july_cohort.append("20")
+july_cohort.append("30")
+july_cohort.append("40")
+print(july_cohort)  
+
+july_cohort[1] = "50","60","70"
+print(july_cohort)
+july_cohort[1]= "15"
+for i in range(len(july_cohort)):
+    if i == len:'july_cohort'    
+print(july_cohort)
+
+july_cohort.pop()
+print(july_cohort)
+last_item = july_cohort[:-1]
+print(last_item)
+numbers = ["10","15","30","40","50","60","70"]
+numbers.sort()
+print(numbers)
+
+print(july_cohort)
+
+for i in range(len(july_cohort)):
+    if july_cohort[1] == "50":
+        print  
